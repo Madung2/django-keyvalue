abbed4a26b8611479bdae1b41a3a40a66443f59e
Madung2 <tuliphan91@gmail.com>
2024-08-02 23:57:35 +0900

    fix

--- docTransformer/TsExpert/urls.py
+++ docTransformer/TsExpert/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import extract_key_value, extract_xl, render_tsexpert, save_key_value, render_docGenerator, run_generator, get_task_status
+from .views import extract_key_value, extract_xl, render_tsexpert, save_key_value, render_docGenerator, run_generator, run_generator_data, get_task_status
 urlpatterns = [
     path('', render_tsexpert, name='render_tsexpert'),
     path('gen/', render_docGenerator, name='render_docGenerator'),
@@ -9,6 +9,7 @@ urlpatterns = [
     path('extract_xl/', extract_xl, name='extract_xl'),
     path('save_key_value/', save_key_value, name='save_key_value'),
     path('run_generator/', run_generator, name='run_generator'),
+    path('run_generator_data/', run_generator_data, name='run_generator_data'),
     path('task_status/<str:task_id>/', get_task_status, name='get_task_status'),
 ]
 
